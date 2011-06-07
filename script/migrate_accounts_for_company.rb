917c9ced76f0fc1e035eae21b4b46927295298f2
Luis E. Guardiola <lguardiola@gmail.com>
2011-06-07 18:13:37 -0300

    add verbose to migration script for accounts

--- script/migrate_accounts_for_company.rb
+++ script/migrate_accounts_for_company.rb
@@ -11,16 +11,17 @@ file_to_migrate = ARGV.last
 #CSV.foreach(file_to_migrate,:col_sep => ';') do |row|
 CSV.foreach(file_to_migrate) do |row|
 	code, name, parent_code = row
-	unless parent_code.blank? || parent_code.to_i.zero?
+	puts "retrive code: #{code}, name: #{name}, parent_code: #{parent_code} ..."
+	unless parent_code.blank? || parent_code.to_i.zero? 
 	  parent_account = Account.scoped_by_company_id(company_id).find_by_code(parent_code)
-	  $stderr.puts "ERROR: parent error code for #{parent_code} on #{name.try(:titlecase)} account and code #{code}!" if parent_account.nil?
+	  puts "ERROR: parent error code for #{parent_code} on #{name.try(:titlecase)} account and code #{code}!" if parent_account.nil?
 	end
 
-	$stderr.puts "creating account #{name.try(:titlecase)} ..."
+	puts "creating account #{name.try(:titlecase)} ..."
 	account = Account.create(:company_id => company_id, :code => code, :name => (name.encode.try(:titlecase) || code))
 	
 	unless parent_account.nil?
-		$stderr.puts "moving account #{account.name.titlecase} to child of #{parent_account.name.try(:titlecase)}..."
+		puts "moving account #{account.name.titlecase} to child of #{parent_account.name.try(:titlecase)}..."
 		account.move_to_child_of(parent_account) 
 	end
 end
