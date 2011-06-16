73a9a0cc71c4fe6b96c993ec9d597b8154d73307
luis petek <lmpetek@gmail.com>
2011-06-16 18:49:02 -0300

    pasaje de pacientes lujan

---
+++ script/migrate_pacientes_lujan.rb
@@ -0,0 +1,23 @@
+unless ARGV.size == 2
+	$stderr.puts "USAGE: rails runner #{$0} [company id] [path/to/csv/file]"
+	exit(1)
+end
+
+require 'csv'
+
+company_id = ARGV.first
+file_to_migrate = ARGV.last
+
+#CSV.foreach(file_to_migrate,:col_sep => ';') do |row|
+CSV.foreach(file_to_migrate) do |row|
+	code, name, tutor, direccion,localidad, provincia, codpos, codposx, telefono = row
+	puts "retrive code: #{code}, name: #{name} ..."
+
+	puts "creating customer #{name.try(:titlecase)} ..."
+	Cliente.create!(:empresa_id => company_id,
+	                :codigo => code ,
+	                :razonsocial => (name.encode.try(:titlecase)),
+	                :direccion => direccion,
+	                :telefono => telefono)
+	
+end
