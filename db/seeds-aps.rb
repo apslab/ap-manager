afc9e207d854860f55df31780a1abcbeeed3f72b
luis petek <lmpetek@gmail.com>
2011-07-08 12:01:25 -0300

    siembra de datos para aps

--- db/seeds-aps.rb
+++ db/seeds-aps.rb
@@ -6,29 +6,29 @@ Execute:
   load Rails.root.join('db/seeds-cnsp.rb').to_s
 =end
 
-aps_companies = ['APSystems srl', 'APS Prueba']
+cnsp_companies = ['Casa Nuestra Señora del Pilar', 'CNSP Prueba']
 
-aps_users = [{
-    :email => 'agustin@ap-sys.com.ar',
-    :first_name => 'Agustin',
-    :last_name => 'Annecchini'
+cnsp_users = [{
+    :email => 'serafin.hernandez@sanjuandedios.org.ar',
+    :first_name => 'Cacho',
+    :last_name => 'Hernandez'
   },
   {
-    :email => 'luis@ap-sys.com.ar',
-    :first_name => 'luis',
-    :last_name => 'petek'
+    :email => 'myriam.zeppa@sanjuandedios.org.ar',
+    :first_name => 'Myriam',
+    :last_name => 'Zeppa'
   },
   {
-    :email => 'claudio@ap-sys.com.ar',
-    :first_name => 'claudio',
-    :last_name => 'annecchini'
+    :email => 'enrique.vanessche@sanjuandedios.org.ar',
+    :first_name => 'Enrique',
+    :last_name => 'Vanessche'
   }]
 
-aps_users.each do |cnsp_user|
+cnsp_users.each do |cnsp_user|
   $stderr.puts("create user #{cnsp_user[:email]} with password 123456 and activate...")
-  User.create!(:email => aps_user[:email],
-               :last_name => aps_user[:first_name],
-               :first_name => aps_user[:last_name],
+  User.create!(:email => cnsp_user[:email],
+               :last_name => cnsp_user[:first_name],
+               :first_name => cnsp_user[:last_name],
                :password => '123456', 
                :password_confirmation => '123456').confirm!
 end
@@ -40,12 +40,12 @@ cnsp_companies.each do |cnsp_company|
 end
 =end
 
-users = User.where(:email => aps_users.map{|aps_user|aps_user[:email]})
-Company.where(:name => aps_companies).each do |company|
+users = User.where(:email => cnsp_users.map{|cnsp_user|cnsp_user[:email]})
+Company.where(:name => cnsp_companies).each do |company|
   users.each do |user|
     $stderr.puts("create memberships for #{user.email}...")
     membership = Membership.create!(:company_id => company.id,:role_id => Role.first.id, :user_id => user.id)
-    if company.name == aps_companies.first
+    if company.name == cnsp_companies.first
       $stderr.puts("setting membership\'s current company to #{company.name} for #{user.email}...")
       membership.toggle!(:current)  
     end       
